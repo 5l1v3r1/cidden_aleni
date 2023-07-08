a2747217eb4d22e924d1ff59813b878696301381
avakado0 <avakadobatu@gmail.com>
2023-07-08 05:18:01 +0300

    Begun checking xpaths

---
+++ .history/src/cddn_aln/cddn_aln/items_20230708044809.py
@@ -0,0 +1,30 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class Karar(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    application_number = scrapy.Field
+    application_title = scrapy.Field
+    application_date = scrapy.Field
+    deciding_unit = scrapy.Field
+    decision_result = scrapy.Field
+    official_gazette_no = scrapy.Field
+    have_press_releases = scrapy.Field
+    alleged_violation = scrapy.Field
+    right_liberty_at_stake = scrapy.Field
+    application_title = scrapy.Field
+    application_topic = scrapy.Field
+    accessed_timestamp = scrapy.Field
+    kunye = scrapy.Field
+    karar_text = scrapy.Field
+class KararBilgiFormu(scrapy.Item):
+
+
+
+
